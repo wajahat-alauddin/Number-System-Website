8932b4d191b789c74563a1125ac4d16b5dd2f919
Muhammd Wajahat <wajahat4c@gmail.com>
2020-08-26 23:19:11 +0500

    completed conversion js

--- scripts/hexadecimal.js
+++ scripts/hexadecimal.js
@@ -1,22 +1,135 @@
-
-
-class Hexadecimal
+function toBinary(number)
 {
-	constructor(){
-        this.num="";
-    }
-    constructor(num){
-        this.num=num;
-    }
-	setNumber(num);
-	setNumber();
-	getNumber();
-	toBinary();
-	toDecimal();
-	toOctal();
-	toHexadecimal();
-	add(Hexadecimal);
-	subtract(Hexadecimal);
-	multiply(Hexadecimal);
-	divide(Hexadecimal);
-};
+	var bin = "";
+	for (var i = 0; i < number.length; i++)
+	{
+		var temp = "";
+		var x = ((number[i].codePointAt(0))-48);
+		if (x > 9)
+		{
+			x = ((number[i].codePointAt(0))-55);
+		}
+		while (x != 0)
+		{
+			var c = parseInt(x % 2);
+			if (c != 0)
+				temp = temp + "1";
+			else
+				temp = temp + "0";
+			x = parseInt(x / 2);
+		}
+		//before inserting, reverses the string 
+		//to get correct Binary value:
+		temp = temp.split("").reverse().join("");
+		if (temp.length === 3)
+		{
+			temp="0"+temp;
+		}
+		else if (temp.length === 2)
+		{
+			temp="00"+temp;
+		}
+		else if (temp.length === 1)
+		{
+			temp="000"+temp;
+		}
+		else if (temp.length === 0)
+		{
+			temp="0000";
+		}
+		bin = bin + temp;
+	}
+	if (bin.length === 0)
+	{
+		return "0";
+	}
+	return bin;
+}
+function toDecimal(number)
+{
+	var dec = 0;
+	var j = 0;
+	for (var i = number.length - 1; i >= 0; i--)
+	{
+		var hexPosValue = ((number[i].codePointAt(0))-48);
+		if (hexPosValue > 9)
+		{
+			hexPosValue = ((number[i].codePointAt(0))-55);
+		}
+		dec += parseInt(Math.pow(16, j)) * hexPosValue;
+		j++;
+	}
+	return dec;
+}
+function toOctal(number)
+{
+	var dec = toDecimal(number);
+	var oct = "";
+	if(dec===0)
+	{
+		return "0";
+	}
+	else
+	{
+		var j = 0;
+		//Decimal to Oct:
+		while (dec != 0)
+		{
+			var remainder = parseInt(dec % 8);
+			oct = oct + String(remainder);
+			dec = parseInt(dec / 8);
+		}
+		//reverses the string to get correct Octal value
+		return oct.split("").reverse().join("");
+	}
+}	
+function toHexadecimal(number)
+{
+	var num = parseInt(number);
+	var hex = "";
+	if (num === 0)
+	{
+		return "0";
+	}
+	while (num != 0)
+	{
+		var remainder = parseInt(num % 16);
+		if (remainder > 9)
+		{
+			hex = hex + String.fromCharCode(remainder + 55);
+			num = parseInt(num / 16);
+			continue;
+		}
+		hex = hex + String(remainder);
+		num = parseInt(num / 16);
+	}
+	//reverses the string to get correct Hexadecimal value
+	return hex.split("").reverse().join("");
+}
+
+$(function () {
+	var value;
+	var valid=true;
+	$("#input-hex").on("input", function () {
+		value=$("#input-hex").val();
+	});    
+	$("#input-hex").on("keypress", function (event) {
+		valid=true;
+		if(event.which===13){
+			for(var i=0;i<value.length;i++){
+				if ((value[i].codePointAt(0))-48 > 9 && (value[i].codePointAt(0))-55 > 15){
+					valid=false;
+				}
+			}
+		if(valid)
+		{	
+			$("#outbox-bin").html(toBinary(value));
+			$("#outbox-dec").html(toDecimal(value));
+			$("#outbox-oct").html(toOctal(value));
+		}
+		else{
+			alert("incorrect entry please enter valid number."); 
+		}
+	}
+	});
+});
--- scripts/octal.js
+++ scripts/octal.js
@@ -1,6 +1,3 @@
-function insert(str, index, value) {
-    return str.substr(0, index) + value + str.substr(index);
-}
 	function toBinary(number)
 	{
 		var bin = "";
@@ -8,56 +5,122 @@ function insert(str, index, value) {
 		{
 			var temp = "";
 			var a = parseInt(number[i]);
-			console.log(a);
 			while (a != 0)
 			{
-				var c = a % 2;
-				if (c != 0)
+				var c = parseInt(a % 2);
+				if (c != 0) {
 					temp = temp + "1";
+				}
 				else
+				{
 					temp = temp + "0";
-				a = a / 2;
+				}
+				a = parseInt(a / 2);
 			}
 			//before inserting, reverses the string 
 			//to get correct Binary value:
-			temp.split("").reverse().join("");
-			console.log(temp);
+			temp=temp.split("").reverse().join("");
 			if (temp.length === 2)
 			{
-				insert(temp,0, "0");
+				temp="0"+temp;
 			}
 			else if (temp.length === 1)
 			{
-				insert(temp,0, "00");
+				temp="00"+temp;
 			}
-			console.log(temp);
+			else if (temp.length === 0)
+			{
+				temp="000"+temp;
+			}
 			bin = bin + temp;
 		}
 		if (bin.length === 0)
 		{
-			return "0";
+			return "000";
 		}
 		return bin;
 	}
-	function toDecimal()
+	function toDecimal(number)
 	{
 		var dec = 0;
-		var j = 0;
-		for (var i = (this.num.length - 1); i >= 0; i--)
+		var position = 0;
+		for (var i = (number.length - 1); i >= 0; i--)
 		{
-			if (num[i] != '0')
-			{
-				dec += parseInt(Math.pow(2, j));
-			}
-			j++;
+			var octPosValue = parseInt(number[i]);
+			dec += parseInt(Math.pow(8, position)) * octPosValue;
+			position++;
 		}
 		return dec;
-	}	
-		// toOctal();
-		// toHexadecimal();
-		// add(Octal);
-		// subtract(Octal);
-		// multiply(Octal);
-		// divide(Octal);
-	var oct="663";
-	console.log(toBinary(oct));
+	}
+	function toOctal(number)
+	{
+		var num = parseInt(number);
+		if (num === 0)
+		{
+			return "0";
+		}
+		var oct = "";
+		while (num != 0)
+		{
+			var c = parseInt(num % 8);
+			oct = oct + String(c);
+			num = parseInt(num / 8);
+		}
+		//reverses the string to get correct Octal value:
+		return oct.split("").reverse().join("");
+	}
+	function toHexadecimal(number)
+	{
+	
+		var dec = toDecimal(number);
+		var hex = "";
+		if (dec === 0)
+		{
+			return ("0");
+		}
+		else
+		{
+			//Octal to Hex: through Dec 
+			while (dec != 0)
+			{
+				var remainder = parseInt(dec % 16);
+				if (remainder > 9)
+				{
+					hex = hex + String.fromCharCode(remainder + 55);
+					dec = parseInt(dec / 16);
+					continue;
+				}
+				hex = hex + String(remainder);
+				dec = parseInt(dec / 16);
+			}
+			//reverses the string to get correct Hexadecimal value:
+			return hex.split("").reverse("").join("");
+		}
+	}
+		$(function () {
+			var value;
+			var valid=true;
+			$("#input-oct").on("input", function () {
+				value=$("#input-oct").val();
+			});    
+			$("#input-oct").on("keypress", function (event) {
+				valid=true;
+				if(event.which===13){
+					for(var i=0;i<value.length;i++){
+						if(parseInt(value) < 0 || parseInt(value[i]) ===NaN || parseInt(value[i]) > 7 ){
+
+							valid=false;
+						}
+					}
+				if(valid)
+				{	
+					$("#outbox-bin").html(toBinary(value));
+					$("#outbox-dec").html(toDecimal(value));
+					$("#outbox-hex").html(toHexadecimal(value));
+				}
+				else{
+					alert("incorrect entry please enter valid number."); 
+				}
+			}
+			});
+		});
